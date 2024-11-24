d2e5a98955e8f8dd0b7c5fbd72e2d5528847a022
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-11-24 15:04:14 +0530

    server.js done to start

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require ('express');
+const mongoose = require('mongoose');   
+
+//  express app
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next() 
+})
+
+// database connectivity 
+mongoose.connect(process.env.MONG_URI)
+    .then(()=>{
+
+    // listninng to thw port
+    app.listen(process.env.PORT, ()=>{
+       console.log('Successfully connect to the DB & listning to the port : ',process.env.PORT);
+}) 
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
